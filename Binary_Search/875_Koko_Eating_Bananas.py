cb04437f8f7a2f2046caaa35a03d50de1b3c479a
imbelal <imbelalhosen@gmail.com>
2023-02-01 21:23:11 +0100

    875 added

---
+++ Binary_Search/875_Koko_Eating_Bananas.py
@@ -0,0 +1,20 @@
+from ast import List
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
+        l, r = 1, max(piles)
+        res = r
+        while l <= r:
+            mid = (l + r) // 2
+            hourRequired = 0
+            for x in piles:
+                hourRequired += ceil(x/mid)
+            if h < hourRequired:
+                l = mid + 1
+            else:
+                res = min(r, mid)
+                r = mid - 1
+
+        return res
