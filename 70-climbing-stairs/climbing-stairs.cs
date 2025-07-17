5a3897e346df56e923ae5e88fc0d4888b83cf7bb
Md Belal Hosen <91498577+imbelal@users.noreply.github.com>
2025-07-18 01:40:11 +0200

    Time: 0 ms (100.00%) | Memory: 28.7 MB (95.23%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cs
+++ 70-climbing-stairs/climbing-stairs.cs
@@ -1,16 +1,15 @@
 public class Solution {
-    int[] cache;
     public int ClimbStairs(int n) {     
-        cache = new int[n];
-        for (int i = 0; i < n; i++) {
-            cache[i] = -1;
-        } 
-        return Dfs(n, 0);
-    }
-
-    private int Dfs(int n, int i){
-        if(i >= n) return i == n ? 1 : 0;
-        if (cache[i] != -1) return cache[i];
-        return cache[i] = Dfs(n, i+1) + Dfs(n, i+2);
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
