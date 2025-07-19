90123f46552cc40593628ed730357b3cdad26a9e
Md Belal Hosen <91498577+imbelal@users.noreply.github.com>
2025-07-19 20:11:27 +0200

    Time: 0 ms (100.00%) | Memory: 41.4 MB (34.41%) - LeetSync

--- 198-house-robber/house-robber.cs
+++ 198-house-robber/house-robber.cs
@@ -1,20 +1,16 @@
 public class Solution {
     public int Rob(int[] nums) {
-        int[] memo = new int[nums.Length];
-        Array.Fill(memo, -1);
-        
-        int t = Dfs(0);
+        if (nums.Length == 0) return 0;
+        if (nums.Length == 1) return nums[0];
 
-        int Dfs(int i){
-            if(i >= nums.Length)
-                return 0;
-            if(memo[i] != -1)
-                return memo[i];
-                
-            memo[i] = Math.Max(nums[i] + Dfs(i+2), Dfs(i+1));
-            return memo[i];
+        int[] dp = new int[nums.Length];
+        dp[0] = nums[0];
+        dp[1] = Math.Max(nums[0], nums[1]);
+
+        for(int i=2; i < nums.Length; i++){
+            dp[i] = Math.Max(dp[i-1], nums[i] + dp[i-2]);
         }
 
-        return t;
+        return dp[nums.Length - 1];
     }
 }
